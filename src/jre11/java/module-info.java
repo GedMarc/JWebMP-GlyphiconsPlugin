4d58d9190f297e5338957376af52148ddf379b8f
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:32:10 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,7 +3,6 @@ module com.jwebmp.plugins.glyphicons {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
